22e2321828ae104a3f79f17ce126d6624b405502
jiangcheng <jiangcheng05@sinochem.com>
2018-05-14 21:03:37 +0800

    关于jdk开发的项目

---
+++ src/main/java/com/chanspace/App.java
@@ -0,0 +1,33 @@
+package com.chanspace;
+
+import lombok.Data;
+
+/**
+ * Hello world!
+ *
+ */
+@Data
+public class App 
+{
+	
+	private String roofsName;
+	
+	private String roofsCode;
+	
+	private Workers workers;
+	
+	
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+
+
+	public App(String roofsName, String roofsCode) {
+		super();
+		this.roofsName = roofsName;
+		this.roofsCode = roofsCode;
+	}
+}
