caa762fcf15c723df62e47b209f6324db9539f8b
jiangcheng <jiangcheng05@sinochem.com>
2018-05-22 12:30:29 +0800

    测试lambda表达式

--- src/main/java/com/chanspace/lambda/LambdaTest.java
+++ src/main/java/com/chanspace/lambda/LambdaTest.java
@@ -1,8 +1,10 @@
 package com.chanspace.lambda;
 
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * 类名称：LambdaTest<br>
@@ -57,7 +59,7 @@ public class LambdaTest {
     /**
      * 使用lambda表达式和函数式接口Predicate
      */
-    public void testFilter(){
+    public void testCondition(){
         List<String> languages = Arrays.asList("Java", "Scala", "C++", "Haskell", "Lisp");
 
         System.out.println("Languages which starts with J :");
@@ -105,7 +107,7 @@ public class LambdaTest {
     /**
      * Java 8中使用lambda表达式的Map和Reduce示例
      */
-    public void testMain(){
+    public void testMapResuce(){
         System.out.println("jdk7-:");
         // 不使用lambda表达式为每个订单加上12%的税
         List<Integer> costBeforeTax = Arrays.asList(100, 200, 300, 400, 500);
@@ -113,7 +115,60 @@ public class LambdaTest {
             double price = cost + .12*cost;
             System.out.println(price);
         }
+        double total = 0;
+        for (Integer cost : costBeforeTax) {
+            double price = cost + .12*cost;
+            total = total + price;
+        }
+        System.out.println("Total : " + total);
         System.out.println("jdk8+:");
         costBeforeTax.stream().map((cost)->cost+.12*cost).forEach(System.out::println);
+
+        double bill = costBeforeTax.stream().map((cost)->cost+0.12*cost).reduce((sum,cost)->sum+cost).get();
+        System.out.println("Total:"+bill);
+    }
+
+    /**
+     * filter
+     */
+    public void testFilter(){
+        List<String> strList = Arrays.asList("abc","","bcd","","defg","jk");
+        // 创建一个字符串列表，每个字符串长度大于2
+        List<String> filtered = strList.stream().filter(x->x.length()>2).collect(Collectors.toList());
+
+        System.out.printf("Original List : %s, filtered list : %s %n", strList, filtered);
+    }
+
+    /**
+     * function
+     */
+    public void testJoining(){
+        // 将字符串换成大写并用逗号链接起来
+        List<String> G7 = Arrays.asList("USA", "Japan", "France", "Germany", "Italy", "U.K.","Canada");
+        String G7Countries = G7.stream().map(x->x.toUpperCase()).collect(Collectors.joining(","));
+        System.out.println(G7Countries);
+    }
+
+    /**
+     * dstinct
+     */
+    public void testDistinct(){
+        // 用所有不同的数字创建一个正方形列表
+        List<Integer> numbers = Arrays.asList(9, 10, 3, 4, 7, 3, 4);
+        List<Integer> distinct = numbers.stream().map(i->i*i).distinct().collect(Collectors.toList());
+        System.out.printf("Original List : %s,  Square Without duplicates : %s %n", numbers, distinct);
+    }
+
+    /**
+     * function
+     */
+    public void testMain(){
+        //获取数字的个数、最小值、最大值、总和以及平均值
+        List<Integer> primes = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
+        IntSummaryStatistics stats = primes.stream().mapToInt((x)->x).summaryStatistics();
+        System.out.println("Highest prime number in List : "+stats.getMax());
+        System.out.println("Lowest prime number in List : "+stats.getMin());
+        System.out.println("Sum of all prime numbers : "+stats.getSum());
+        System.out.println("Average of all prime numbers : "+stats.getAverage());
     }
 }
