dc280ae8a6b1faa49b723a61aa4badc4a55b6079
jiangcheng <jiangcheng05@sinochem.com>
2018-07-16 14:50:34 +0800

    与和或的用例新增二进制显示

--- src/main/java/com/chanspace/bitwise/AndOperation.java
+++ src/main/java/com/chanspace/bitwise/AndOperation.java
@@ -13,6 +13,11 @@ public class AndOperation {
     public static void main(String[] args) {
         int a = 129;
         int b = 128;
+
+        System.out.println(" a 的二进制形态是 " + Integer.toBinaryString(a));
+        System.out.println(" b 的二进制形态是 " + Integer.toBinaryString(b));
         System.out.println(" a and b result is :" + (a&b));
+        System.out.println(" a 与 b 的二进制形态是 : " + Integer.toBinaryString(a&b));
+
     }
 }
--- src/main/java/com/chanspace/bitwise/OrOperation.java
+++ src/main/java/com/chanspace/bitwise/OrOperation.java
@@ -13,6 +13,11 @@ public class OrOperation {
     public static void main(String[] args) {
         int a = 129;
         int b = 128;
+
+        System.out.println(" a 的二进制形态是 " + Integer.toBinaryString(a));
+        System.out.println(" b 的二进制形态是 " + Integer.toBinaryString(b));
         System.out.println("a or b result is : " + (a|b));
+        System.out.println(" a 或 b 的二进制形态是 : " + Integer.toBinaryString(a|b));
+
     }
 }
