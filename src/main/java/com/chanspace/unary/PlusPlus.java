15cdf90c67d5542712e95ebdb65df8ec3ab4abbb
jiangcheng <jiangcheng05@sinochem.com>
2018-07-16 15:10:21 +0800

    一元运算符

---
+++ src/main/java/com/chanspace/unary/PlusPlus.java
@@ -0,0 +1,26 @@
+package com.chanspace.unary;
+
+/**
+ * 类名称：PlusPlus<br>
+ * 类描述：<br>
+ * 创建时间：2018年07月16日<br>
+ *
+ * @author jiangcheng
+ * @version 1.0.0
+ */
+
+public class PlusPlus {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        int sum = a+ ++b;
+        System.out.println("a=" + a + ",b="+b+",sum="+sum);
+
+
+
+        int a1 = 10;
+        int b1 = 10;
+        int sum1 = a1+++b1;
+        System.out.println("a1 = " + a1 + ",b="+b1+",sum1 =" + sum1);
+    }
+}
