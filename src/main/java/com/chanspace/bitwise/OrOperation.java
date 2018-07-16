d62fb5db8dfa88f0e47eaab62381726e9db38808
jiangcheng <jiangcheng05@sinochem.com>
2018-07-16 14:30:53 +0800

    或运算结果129&128还是129

---
+++ src/main/java/com/chanspace/bitwise/OrOperation.java
@@ -0,0 +1,18 @@
+package com.chanspace.bitwise;
+
+/**
+ * 类名称：OrOperation<br>
+ * 类描述：<br>
+ * 创建时间：2018年07月16日<br>
+ *
+ * @author jiangcheng
+ * @version 1.0.0
+ */
+
+public class OrOperation {
+    public static void main(String[] args) {
+        int a = 129;
+        int b = 128;
+        System.out.println("a or b result is : " + (a|b));
+    }
+}
