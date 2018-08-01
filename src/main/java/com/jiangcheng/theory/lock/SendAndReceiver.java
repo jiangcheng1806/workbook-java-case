97d5d0c9077e71bf8b91a9655ff2c82c122cfeed
jiangcheng <jiangcheng05@sinochem.com>
2018-08-01 15:32:50 +0800

    高级同步机制的应用场景

---
+++ src/main/java/com/jiangcheng/theory/lock/SendAndReceiver.java
@@ -0,0 +1,46 @@
+package com.jiangcheng.theory.lock;
+
+import java.util.concurrent.Exchanger;
+
+/**
+ * 类名称：SendAndReceiver<br>
+ * 类描述：对象交换器<br>
+ * 创建时间：2018年08月01日<br>
+ *
+ * @author jiangcheng
+ * @version 1.0.0
+ */
+
+public class SendAndReceiver {
+    private final Exchanger<StringBuilder> exchanger = new Exchanger<>();
+    private class Sender implements Runnable{
+
+        @Override
+        public void run() {
+            try{
+                StringBuilder content = new StringBuilder("Hello");
+                content = exchanger.exchange(content);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    private class Receiver implements Runnable{
+
+        @Override
+        public void run() {
+            try {
+                StringBuilder content = new StringBuilder("World");
+                content = exchanger.exchange(content);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    public void exchange(){
+        new Thread(new Sender()).start();
+        new Thread(new Receiver()).start();
+    }
+}
