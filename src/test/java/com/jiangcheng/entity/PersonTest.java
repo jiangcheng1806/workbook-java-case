74b2859ef441a1f7cdea31e0115af8345a768fc1
jiangcheng <jiangcheng05@sinochem.com>
2018-08-08 16:56:38 +0800

    Spring中的依赖注入方式,byType和byName两种，使用Person类和car类进行测试

---
+++ src/test/java/com/jiangcheng/entity/PersonTest.java
@@ -0,0 +1,23 @@
+package com.jiangcheng.entity;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import static org.junit.Assert.*;
+
+/**
+ * 类名称：PersonTest<br>
+ * 类描述：<br>
+ * 创建时间：2018年08月08日<br>
+ *
+ * @author jiangcheng
+ * @version 1.0.0
+ */
+public class PersonTest {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        Person person = (Person) applicationContext.getBean("person");
+        System.out.println(person);
+    }
+
+}
