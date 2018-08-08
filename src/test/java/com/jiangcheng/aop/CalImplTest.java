48c5ae01f63e950fd08f4d21e08df8f45c485f60
jiangcheng <jiangcheng05@sinochem.com>
2018-08-08 17:51:31 +0800

    动态代理的使用，实现InvocationHandler,包含被代理对象并能返回被代理对象

---
+++ src/test/java/com/jiangcheng/aop/CalImplTest.java
@@ -0,0 +1,26 @@
+package com.jiangcheng.aop;
+
+import static org.junit.Assert.*;
+
+/**
+ * 类名称：CalImplTest<br>
+ * 类描述：<br>
+ * 创建时间：2018年08月08日<br>
+ *
+ * @author jiangcheng
+ * @version 1.0.0
+ */
+public class CalImplTest {
+    public static void main(String[] args) {
+        //被代理对象
+        Cal cal = new CalImpl();
+        MyInvocationHandler mh = new MyInvocationHandler();
+        //代理对象
+        Cal cal1 = (Cal) mh.bind(cal);
+        cal1.add(10,3);
+        cal1.sub(10,3);
+        cal1.mul(10,3);
+        cal1.div(10,3);
+    }
+
+}
