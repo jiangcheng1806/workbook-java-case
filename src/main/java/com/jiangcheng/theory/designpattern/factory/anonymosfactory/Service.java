65d626ecc65d0613dbdf95dd4d0e348914b3db4e
jiangcheng <jiangcheng05@sinochem.com>
2018-08-18 10:52:58 +0800

    通过匿名类实现的简化抽象工厂模式

---
+++ src/main/java/com/jiangcheng/theory/designpattern/factory/anonymosfactory/Service.java
@@ -0,0 +1,14 @@
+package com.jiangcheng.theory.designpattern.factory.anonymosfactory;
+
+/**
+ * 类名称：Service<br>
+ * 类描述：<br>
+ * 创建时间：2018年08月17日<br>
+ *
+ * @author jiangcheng
+ * @version 1.0.0
+ */
+public interface Service {
+    void method1();
+    void method2();
+}
