aadaccfebc40755283d2a7b4604206bc1337d5db
jiangcheng <jiangcheng05@sinochem.com>
2018-08-27 16:05:21 +0800

    代码优化

--- src/main/java/com/jiangcheng/theory/designpattern/proxy/daoproxy/StaticProxyTest.java
+++ src/main/java/com/jiangcheng/theory/designpattern/proxy/daoproxy/StaticProxyTest.java
@@ -5,7 +5,7 @@ import com.jiangcheng.dao.impl.UserDAOImpl;
 import org.junit.Test;
 
 /**
- * 类名称：StaticUserProxy<br>
+ * 类名称：StaticProxyTest<br>
  * 类描述：<br>
  * 创建时间：2018年08月27日<br>
  *
@@ -13,7 +13,7 @@ import org.junit.Test;
  * @version 1.0.0
  */
 
-public class StaticUserProxy {
+public class StaticProxyTest {
     @Test
     public void testStaticProxy(){
         //目标对象
---
+++ src/main/java/com/jiangcheng/theory/designpattern/proxy/daoproxy/UserProxyFactory.java
@@ -0,0 +1,14 @@
+package com.jiangcheng.theory.designpattern.proxy.daoproxy;
+
+/**
+ * 类名称：UserProxyFactory<br>
+ * 类描述：<br>
+ * 创建时间：2018年08月27日<br>
+ *
+ * @author jiangcheng
+ * @version 1.0.0
+ */
+
+public class UserProxyFactory {
+
+}
