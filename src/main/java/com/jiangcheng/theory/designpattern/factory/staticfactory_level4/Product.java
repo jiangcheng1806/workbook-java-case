20c13e4ac59c36c7fbbb16d9ca6029fe0a8b1c9c
jiangcheng <jiangcheng05@sinochem.com>
2018-08-28 10:58:21 +0800

    新增一个特殊的静态工厂 可以说是静态工厂和抽象工厂的结合体 加强版 直接用类当参数 使其复用性更强 个人感觉是最强的工厂模式

---
+++ src/main/java/com/jiangcheng/theory/designpattern/factory/staticfactory_level4/Product.java
@@ -0,0 +1,13 @@
+package com.jiangcheng.theory.designpattern.factory.staticfactory_level4;
+
+/**
+ * 类名称：Product<br>
+ * 类描述：<br>
+ * 创建时间：2018年08月28日<br>
+ *
+ * @author jiangcheng
+ * @version 1.0.0
+ */
+
+public class Product {
+}
