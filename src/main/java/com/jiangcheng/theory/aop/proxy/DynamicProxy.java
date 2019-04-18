60ecb883714622c5632cdb06be8ba45059d58e37
jiangcheng <jiangcheng05@sinochem.com>
2019-04-18 10:52:38 +0800

    代码优化 #无 优化：代码优化

---
+++ src/main/java/com/jiangcheng/theory/aop/proxy/DynamicProxy.java
@@ -0,0 +1,42 @@
+package com.jiangcheng.theory.aop.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * 类名称：DynamicProxy<br>
+ * 类描述：<br>
+ * 创建时间：2019年04月04日<br>
+ *
+ * @author jiangcheng
+ * @version 1.0.0
+ */
+
+public class DynamicProxy {
+}
+
+interface Subject{
+    void action();
+}
+
+//被代理类
+class RealSubject implements Subject {
+
+    @Override
+    public void action() {
+        System.out.println("被代理类执行");
+    }
+}
+
+class MyInvocationHandler2 implements InvocationHandler {
+
+    //接口被代理对象的声明
+    Object object;
+
+    //给被代理
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return null;
+    }
+}
