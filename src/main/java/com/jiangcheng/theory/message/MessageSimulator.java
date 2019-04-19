50a383cfb3453e000c517117244ddd88ebb982af
jiangcheng <jiangcheng05@sinochem.com>
2019-04-19 17:19:50 +0800

    新增功能 #无 新增：消息机制模拟

---
+++ src/main/java/com/jiangcheng/theory/message/MessageSimulator.java
@@ -0,0 +1,30 @@
+package com.jiangcheng.theory.message;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+/**
+ * 类名称：MessageSimulator<br>
+ * 类描述：消息模拟<br>
+ * 创建时间：2019年04月19日<br>
+ *
+ * @author jiangcheng
+ * @version 1.0.0
+ */
+
+public class MessageSimulator {
+    //消息队列
+    public static ArrayBlockingQueue<Message> messageQueue = new ArrayBlockingQueue<>(100);
+
+    public static void main(String[] args) {
+        WindowSimulator generator = new WindowSimulator(messageQueue);
+
+        //产生消息
+        generator.GenerateMsg();
+
+        //消息循环
+        Message msg = null;
+        while ((msg = messageQueue.poll()) != null){
+            ((MessageProcess)msg.getSource()).doMessage(msg);
+        }
+    }
+}
