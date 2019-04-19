a9932d3d1129632a72aefe424f77542da4d3ed65
jiangcheng <jiangcheng05@sinochem.com>
2019-04-19 10:09:13 +0800

    代码优化 #无 优化：实验 使用 Parameters 注解 来进行测试 单个参数的情况

---
+++ src/main/java/com/jiangcheng/theory/junit/ParametersTest1.java
@@ -0,0 +1,36 @@
+package com.jiangcheng.theory.junit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * 类名称：FibonacciTest<br>
+ * 类描述：使用 Parameter 注解 需要 属性为public 的 并且 去掉多参构造函数<br>
+ * 创建时间：2019年04月18日<br>
+ *
+ * @author jiangcheng
+ * @version 1.0.0
+ */
+@RunWith(Parameterized.class)
+public class ParametersTest1 {
+
+    @Parameterized.Parameters
+    public static Iterable<? extends Object> data(){
+        return Arrays.asList("first test","second test");
+    }
+
+    @Parameterized.Parameter
+    public /*private*/ String fInput;
+
+    @Test
+    public void test(){
+        assertEquals("first test", fInput);
+    }
+
+}
