6048a9405d21a9abf399b6c974022bdd263cfec4
jiangcheng <jiangcheng05@sinochem.com>
2019-08-23 15:49:12 +0800

    修改项目模块名称

---
+++ spring-application/src/main/java/com/jiangcz/service/UserService.java
@@ -0,0 +1,15 @@
+package com.jiangcz.service;
+
+import com.jiangcz.bean.User;
+
+/**
+ * 类名称：UserService<br>
+ * 类描述：<br>
+ * 创建时间：2018年08月08日<br>
+ *
+ * @author jiangcheng
+ * @version 1.0.0
+ */
+public interface UserService {
+    public User getUserById(int id);
+}
