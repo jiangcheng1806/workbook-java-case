77186730e1557b545d5a2471fd93a73ece8a9fa5
jiangcheng <jiangcheng05@sinochem.com>
2019-09-16 20:19:20 +0800

    新增aop用例

---
+++ application-ioc-aop/src/main/java/com/jiangcz/application/iocaop/aop/Advice.java
@@ -0,0 +1,15 @@
+package com.jiangcz.application.iocaop.aop;
+
+import java.lang.reflect.InvocationHandler;
+
+/**
+ * 类名称：Advice<br>
+ * 类描述：<br>
+ * 创建时间：2019年09月16日<br>
+ *
+ * @author jiangcheng
+ * @version 1.0.0
+ */
+
+public interface Advice extends InvocationHandler {
+}
