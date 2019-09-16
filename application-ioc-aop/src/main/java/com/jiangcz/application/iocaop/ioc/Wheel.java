ce61a8c72bf408881d1bd3759730e00680d78abe
jiangcheng <jiangcheng05@sinochem.com>
2019-09-16 20:08:31 +0800

    新增ioc用例

---
+++ application-ioc-aop/src/main/java/com/jiangcz/application/iocaop/ioc/Wheel.java
@@ -0,0 +1,24 @@
+package com.jiangcz.application.iocaop.ioc;
+
+/**
+ * 类名称：Wheel<br>
+ * 类描述：<br>
+ * 创建时间：2019年09月16日<br>
+ *
+ * @author jiangcheng
+ * @version 1.0.0
+ */
+public class Wheel {
+    private String brand;
+    private String specification ;
+
+    // 省略其他不重要代码
+
+    @Override
+    public String toString() {
+        return "Wheel{" +
+                "brand='" + brand + '\'' +
+                ", specification='" + specification + '\'' +
+                '}';
+    }
+}
