496f2315e561bb943823848f26e96ba91a2e7bad
jiangcheng <jiangcheng05@sinochem.com>
2019-12-05 14:05:15 +0800

    nacos 与 openfeign的对接使用 可以运营

---
+++ application-nacos/nacos-feign/src/main/java/com/jiangcz/nacos/feign/RemoteHystrix.java
@@ -0,0 +1,11 @@
+package com.jiangcz.nacos.feign;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RemoteHystrix implements RemoteClient {
+    @Override
+    public String helloNacos() {
+        return "请求超时了";
+    }
+}
