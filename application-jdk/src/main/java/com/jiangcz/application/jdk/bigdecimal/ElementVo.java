1e6588f93d828aa4d28bda03bd3eaa8bcc4d6f86
jiangcheng <jiangcheng05@sinochem.com>
2019-12-12 21:16:21 +0800

    feat: bigdecimal 危险的操作

---
+++ application-jdk/src/main/java/com/jiangcz/application/jdk/bigdecimal/ElementVo.java
@@ -0,0 +1,20 @@
+package com.jiangcz.application.jdk.bigdecimal;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+public class ElementVo {
+
+    /**
+     * 自定义单号集合
+     */
+    private List<String> strList;
+
+    /**
+     * 总量
+     */
+    private BigDecimal total;
+}
