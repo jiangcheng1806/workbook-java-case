6b1d0f88d9686c14982173170906c4ab0f606cb9
jiangcheng <jiangcheng05@sinochem.com>
2019-12-16 19:36:46 +0800

    反射用例 利用反射获取annotation的值

--- application-jdk/src/main/java/com/jiangcz/application/jdk/reflect/construct/A.java
+++ application-jdk/src/main/java/com/jiangcz/application/jdk/reflect/construct/A.java
@@ -3,6 +3,7 @@ package com.jiangcz.application.jdk.reflect.construct;
 import lombok.Data;
 
 @Data
+@MyAnnotation(name = "小灰灰",value = "懒羊羊")
 public class A extends B{
     private String s;
     public A(String s){
---
+++ application-jdk/src/main/java/com/jiangcz/application/jdk/reflect/construct/AnnotationTest.java
@@ -0,0 +1,21 @@
+package com.jiangcz.application.jdk.reflect.construct;
+
+import java.lang.annotation.Annotation;
+
+public class AnnotationTest {
+    public static void main(String[] args) {
+        Class<A> aClass = A.class;
+// 获取所有的注解
+        Annotation[] annotations = aClass.getAnnotations();
+        for (Annotation annotation : annotations) {
+            System.out.println(annotation);
+        }
+
+// 获取指定的注解
+        MyAnnotation myAnnotation = aClass.getAnnotation(MyAnnotation.class);
+        String name = myAnnotation.name();
+        String value = myAnnotation.value();
+        System.out.println(name);
+        System.out.println(value);
+    }
+}
