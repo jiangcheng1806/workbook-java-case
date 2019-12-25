5d140b2353467641de2f57ddd2a3d68e1fbfdae1
jiangcheng <jiangcheng05@sinochem.com>
2019-12-25 09:59:58 +0800

    dcl不安全的场景就是对象的创建过程是先分配内存空间然后实例化 有可能指令重排序之后造成只是分配了空间 还没有实例化 其他线程以为非空 不进行实例化 就去取值 造成错误

--- application-designpattern/src/main/java/com/jiangcz/application/designpattern/singleton/SingletonFull1_4.java
+++ application-designpattern/src/main/java/com/jiangcz/application/designpattern/singleton/SingletonFull1_4.java
@@ -1,9 +1,12 @@
 package com.jiangcz.application.designpattern.singleton;
 
 
+import java.util.Vector;
+
 /**
  * 
  * 其实仍然是DCL的变种
+ * 影子实例的办法为单例对象的属性同步更新
  * @author Chan Kiang
  *
  */
@@ -17,6 +20,12 @@ public class SingletonFull1_4 {
 	 * 
 	 */
 	private static volatile SingletonFull1_4 instance = null;
+
+	private Vector properties = null;
+
+	public Vector getProperties(){
+		return properties;
+	}
 	
 	/**
 	 * 首先私有化构造函数
@@ -40,11 +49,15 @@ public class SingletonFull1_4 {
 	 * 
 	 * @return
 	 */
-	public SingletonFull1_4 getInstance(){
+	public static SingletonFull1_4 getInstance(){
 		if(instance == null){
 			syncInit();
 		}
 		return instance;
 	}
-	
+
+	public void updateProperties(){
+		SingletonFull1_4 shadow = new SingletonFull1_4();
+		properties = shadow.getProperties();
+	}
 }
