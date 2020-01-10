7d0a792f4c15a5d52dd13ed3deb71c27ed105da3
jiangcheng <jiangcheng05@sinochem.com>
2020-01-10 16:39:09 +0800

    cyclicbarrier 跟 countdownlatch 不同点比较

--- application-concurrent/src/main/java/com/jiangcz/application/concurrent/concurrent/CyclicBarrierTest3.java
+++ application-concurrent/src/main/java/com/jiangcz/application/concurrent/concurrent/CyclicBarrierTest3.java
@@ -7,6 +7,16 @@ import java.util.concurrent.CyclicBarrier;
 /**
  * 类名称：CyclicBarrierTest<br>
  * 类描述：栅栏的重用<br>
+ * CyclicBarrier 和 CountDownLatch 的区别
+ * CountDownLatch	CyclicBarrier
+ * 减计数方式	加计数方式
+ * 计算为0时释放所有等待的线程	计数达到指定值时释放所有等待线程
+ * 计数为0时，无法重置	计数达到指定值时，计数置为0重新开始
+ * 调用countDown()方法计数减一，调用await()方法只进行阻塞，对计数没任何影响	调用await()方法计数加1，若加1后的值不等于构造方法的值，则线程阻塞
+ * 不可重复利用	可重复利用
+ *
+ *
+ *
  * 创建时间：2019年04月19日<br>
  *
  * @author jiangcheng
